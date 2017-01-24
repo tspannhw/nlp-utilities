1ca0f94bdea6ff82378f3a78b2bf8edd7e26ddee
Timothy Spann <tspann@hortonworks.com>
2017-01-24 11:47:35 -0500

    Create senti.py

---
+++ senti.py
@@ -0,0 +1,5 @@
+import sys
+ 
+sid = SentimentIntensityAnalyzer()
+ss = sid.polarity_scores(sys.argv[1])
+print('Compound {0} Negative {1} Neutral {2} Positive {3} '.format(ss['compound'],ss['neg'],ss['neu'],ss['pos']))
